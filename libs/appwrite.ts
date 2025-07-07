d791849203027d6c1554c0f2f452e4d55e008a18
Bekzod0206 <davronovbekzod20@gmail.com>
2025-07-08 00:03:33 +0500

    fix for the deploy

--- libs/appwrite.ts
+++ libs/appwrite.ts
@@ -1,6 +1,8 @@
 import { Client, Account, ID, Databases } from 'appwrite';
 const client = new Client();
-client.setProject('6744c028002b94d56a0d');
+client
+  .setEndpoint(process.env.NUXT_PUBLIC_APPWRITE_ENDPOINT || 'https://cloud.appwrite.io/v1')
+  .setProject(process.env.NUXT_PUBLIC_APPWRITE_PROJECT_ID || '6744c028002b94d56a0d')
 
 export const ACCOUNT = new Account(client)
 export const DATABASE = new Databases(client)
